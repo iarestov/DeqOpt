09beb6824472b2a85a10adcb6dcc2a03f4d9543b
Igor Arestov <i.arestov@gmail.com>
2015-11-28 04:53:09 +0300

    after  all we have +~10% time but valid Deq class. OK tune it. Lets start from bottom - Set/Get by Index horible because do unneded allocation - ty to avoid it

--- DeqSort/Program.cs
+++ DeqSort/Program.cs
@@ -135,7 +135,7 @@ namespace DeqSort
         /// Получить элемент деки по номеру
         /// </summary>
         /// <returns>(T)Элемент</returns>
-        public T GetDeqElementByPosition(int position)
+        public T GetDeqElementByPosition(int position) 
         {
             if (position < 0 || position >= _size) throw new IndexOutOfRangeException();
             if(Count == 1)
